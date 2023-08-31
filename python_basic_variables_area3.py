1ae7f7bf92184a090dbbcc1ed215bbb5eeb35f99
rohithandekar639 <143615182+rohithandekar639@users.noreply.github.com>
2023-08-31 13:28:28 +0530

    Add files via upload

---
+++ python_basic_variables_area3.py
@@ -0,0 +1,7 @@
+celsius = float(input("Enter the temperature in Celsius: "))
+
+# Converting Celsius to Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# converted temperature in Fahrenheit
+print("Temperature in Fahrenheit:", fahrenheit)
