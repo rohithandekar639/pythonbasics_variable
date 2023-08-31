0d4d9a6d06ba75640e965e024c4d146fbbe23cf1
rohithandekar639 <143615182+rohithandekar639@users.noreply.github.com>
2023-08-31 09:12:00 +0530

    Add files via upload

---
+++ python_basics_vairable_area.py
@@ -0,0 +1,8 @@
+length = float(input("Enter the length of rectangle "))
+width = float(input("Enter the width of rectangle"))
+
+#calculate the area of rectangle
+
+area= length * width
+
+print("the area of rectangle is ", area)
